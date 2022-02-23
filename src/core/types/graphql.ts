e12190e65940d88ba31af5e571d67a45238f0b96
Edmond O'Flynn <oflynned@tcd.ie>
2022-02-23 15:55:17 +0100

    chore: generate typescript graphql types

---
+++ src/core/types/graphql.ts
@@ -0,0 +1,47 @@
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+export interface IQuery {
+  __typename?: 'IQuery';
+  queryTime(): DateTime | Promise<DateTime>;
+  getUserById(id: string): UserResult | Promise<UserResult>;
+}
+
+export interface IMutation {
+  __typename?: 'IMutation';
+  mutateTime(): DateTime | Promise<DateTime>;
+  createUser(name: string): UserResult | Promise<UserResult>;
+  deleteUserById(id: string): UserResult | Promise<UserResult>;
+}
+
+export interface User {
+  __typename?: 'User';
+  id: string;
+  name: string;
+  createdAt: DateTime;
+}
+
+export interface DeletedUser {
+  __typename?: 'DeletedUser';
+  id: string;
+  deletedAt: DateTime;
+}
+
+export interface InvalidUserInput {
+  __typename?: 'InvalidUserInput';
+  message: string;
+}
+
+export interface UserNotFound {
+  __typename?: 'UserNotFound';
+  message: string;
+}
+
+export type DateTime = Date;
+export type UserResult = User | DeletedUser | UserNotFound | InvalidUserInput;
+type Nullable<T> = T | null;
