9c29430b4dfbbb04117b56c7b5ebf2b6aace3eb0
Edmond O'Flynn <oflynned@tcd.ie>
2022-03-25 14:54:09 +0100

    chore: register health rest module

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from '../domains/user/user.module';
 import { UserGraphqlModule } from './graphql/user/user.graphql.module';
+import { HealthRestModule } from './rest/health/health.rest.module';
 
 @Module({
-  imports: [UserModule, UserGraphqlModule],
-  exports: [UserGraphqlModule],
+  imports: [UserModule, UserGraphqlModule, HealthRestModule],
+  exports: [UserGraphqlModule, HealthRestModule],
 })
 export class GatewayModule {}
